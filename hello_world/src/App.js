1d26e50fa5cd2fe53c74972a4ea4375ac604e5e5
sai-kishore-r <sai.r@vuedata.in>
2021-12-15 17:32:38 +0530

    commit birtday increment

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Birth from "./components/Birth";
 
 function App() {
   return (
     <div className="App">
-     Hello World
+      <Birth />
     </div>
   );
 }
